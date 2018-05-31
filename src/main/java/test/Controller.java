85e2fd0339d5422dff106e366f9dad7b2883812f
rgade <rgade@osius.com>
2018-05-31 11:47:49 +0530

    added files

---
+++ src/main/java/test/Controller.java
@@ -0,0 +1,30 @@
+package test;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+	
+	@RequestMapping( value="/employee", method= RequestMethod.GET, produces = "application/json")
+	@ResponseBody
+	public Employee employee() {
+		
+		Employee emp = new Employee();
+		  emp.setName("emp1");
+		  emp.setDesignation("manager");
+		  emp.setEmpId("1");
+		  emp.setSalary(3000);
+
+		  return emp;
+	}
+	
+	@RequestMapping("/")
+	public String ntg() {
+		return "HELLOW";
+
+	}
+
+}
